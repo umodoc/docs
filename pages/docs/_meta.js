c5fa8a6a9266058a9cfe73066129eb87252dfee7
umodoc <team@umodoc.com>
2024-05-24 23:57:07 +0800

    调整文档目录

--- pages/docs/_meta.js
+++ pages/docs/_meta.js
@@ -1,10 +1,22 @@
 export default {
+	'--guide': {
+		type: 'separator',
+		title: '基础指南'
+	},
+	index: '基本介绍',
+	features: '核心特性',
+	roadmap: '开发计划',
+	changelog: '更新日志',
+	faq: {
+		title: '常见问题',
+		toc: false
+	},
 	'--start': {
 		type: 'separator',
 		title: '快速上手'
 	},
-	index: '介绍',
-	guide: '指南',
+	'getting-started': '快速开始',
+	theme: '主题定制',
 	'--api': {
 		type: 'separator',
 		title: '接口说明'
