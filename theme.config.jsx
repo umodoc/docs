dfbe3b2f0bb45d99878654dfc6f9de863d175597
有墨文档 <contact@umodoc.com>
2025-08-31 01:10:10 +0800

    今天资源从cdn获取

--- theme.config.jsx
+++ theme.config.jsx
@@ -29,7 +29,7 @@ export default {
 				<title>
 					{title} - {router.locale === 'cn' ? 'Umo Editor 在线文档' : 'Umo Editor Documention'}
 				</title>
-				<link rel="icon" href="/images/favicon.png" />
+				<link rel="icon" href="https://s2.umodoc.com/images/favicon.png" />
 				<meta name="description" content={frontMatter.description} />
 				<meta name="msvalidate.01" content="238678E9B05B3DE2E35D891334FC842E" />
 				<script
