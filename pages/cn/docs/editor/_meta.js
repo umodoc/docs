af8f85248fbbfd93eddc3360c6de343d3d22a11e
umodoc <team@umodoc.com>
2026-01-21 12:43:49 +0800

    完善文档

--- pages/cn/docs/editor/_meta.js
+++ pages/cn/docs/editor/_meta.js
@@ -8,7 +8,6 @@ export default {
   options: '配置项',
   events: '事件列表',
   methods: '方法列表',
-  ai: 'AI 文档助手',
   tiptap: 'Tiptap 编辑器',
   locales: '多语言支持',
   themes: '主题定制',
--- pages/en/docs/editor/_meta.js
+++ pages/en/docs/editor/_meta.js
@@ -7,7 +7,6 @@ export default {
   options: 'Configuration',
   events: 'Events',
   methods: 'Methods',
-  ai: 'AI Assistant',
   tiptap: 'Tiptap Editor',
   locales: 'Locales',
   themes: 'Themes',
