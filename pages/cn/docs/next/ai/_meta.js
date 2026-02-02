e943e440d870b4f0dd648578bc426dd21f497237
有墨文档 <contact@umodoc.com>
2026-02-02 23:27:01 +0800

    完善文档

--- pages/cn/docs/next/ai/_meta.js
+++ pages/cn/docs/next/ai/_meta.js
@@ -1,15 +1,21 @@
 export default {
+  start: {
+    type: 'separator',
+    title: '开始',
+  },
   index: '基础介绍',
   quickstart: '快速开始',
   options: '配置及事件',
-  start: {
+  features: {
     type: 'separator',
+    title: 'AI 能力',
   },
   suggestion: 'AI 智能建议',
   assistant: 'AI 文档助手',
   chat: 'AI 聊天助手',
   more: {
     type: 'separator',
+    title: '扩展阅读',
   },
   protocols: '支持的协议',
   flow: '前后端交互',
