b7d1cd3d24e245f35898af53fb3a2599a3de46e2
umodoc <team@umodoc.com>
2026-04-23 11:02:58 +0800

    完善文档

---
+++ pages/cn/docs/next/revision/_meta.js
@@ -0,0 +1,9 @@
+export default {
+  index: '基本介绍',
+  scenarios: '使用场景',
+  'getting-started': '快速开始',
+  concepts: '核心概念',
+  options: '配置项',
+  methods: '方法列表',
+  troubleshooting: '排障指南',
+}
---
+++ pages/en/docs/next/revision/_meta.js
@@ -0,0 +1,9 @@
+export default {
+  index: 'Introduction',
+  'getting-started': 'Getting Started',
+  concepts: 'Core Concepts',
+  scenarios: 'Use Cases',
+  options: 'Options',
+  methods: 'Methods',
+  troubleshooting: 'Troubleshooting',
+}
